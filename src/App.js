09b1704bf7d010a24d7559299a40d93e19985d72
Faizan Azhar <faizan.azhar@tekenable.com>
2024-01-24 14:32:04 +0500

    new comment 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello bhai  {value}</div>;
+  return <div>Hello bhai jan  {value}</div>;
 }
 
 export default App;
